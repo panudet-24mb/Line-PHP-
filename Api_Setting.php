eeade73ed9fdaf203708bbd0ad413cbfd9a22ec9
panudet-24mb <panudet.panumas@gmail.com>
2019-02-19 05:08:16 -0800

    test git

---
+++ Api_Setting.php
@@ -0,0 +1,6 @@
+<?php
+/// การตั้งค่าเกี่ยวกับ bot ใน LINE Messaging API
+define('LINE_MESSAGE_CHANNEL_ID','youchannelid');
+define('LINE_MESSAGE_CHANNEL_SECRET','sercret');
+define('LINE_MESSAGE_ACCESS_TOKEN','token');
+?>
